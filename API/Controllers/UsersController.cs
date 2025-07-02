f29aa75d5cbbc1ada97d96c08e9f99497b1ad6e2
manishrathod01 <manishrathod384@gmail.com>
2025-07-02 19:16:11 +0530

    Unified repo with API and client folders

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+
+public class UsersController(DataContext context) : BaseApiController
+{
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+        return Ok(users);
+    }
+    [Authorize]
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        if (user == null) return NotFound();
+        return Ok(user);
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,23 @@
+using API.Extensions;
+using API.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
